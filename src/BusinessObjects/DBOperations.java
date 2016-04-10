2497d89dcb28becf9052546758f6d39ef73d731b
KurayamiSenpai <zfalgout>
2016-04-10 12:20:12 -0600

    Added abstract class for database operations

---
+++ src/BusinessObjects/DBOperations.java
@@ -0,0 +1,100 @@
+package BusinessObjects;
+import java.sql.ResultSet;
+
+/**
+ * 
+ * @author zfalgout
+ * 
+ *	Has default implementations for non abstract methods, but these methods just
+ *	return the default types. Designed so objects that shouldn't be allowed to access
+ *	those methods will not be able to do anything with them, however, objects
+ *	that can use those methods should still check the access level before access to
+ *	database
+ */
+public abstract class DBOperations 
+{
+	public enum Roles {supervisor, librarian, patron}
+	
+	/**
+	 * Handles connecting to the database
+	 * @return should return true if connection succeeds, otherwise false
+	 */
+	public abstract boolean connectToDB();
+	
+	/**
+	 * Handles checking out a book for the given uid
+	 * @param uid user id of the person checking the book out
+	 * @param ISBN of the book being checked out
+	 * @return true if checkout succeeds, false otherwise
+	 */
+	public boolean checkout(int uid, int ISBN)
+	{
+		return false;
+	}
+	
+	/**
+	 * Handles checking in a book for the given uid
+	 * Should check that the given ISBN is in the checkedOut table
+	 * @param uid user id of the person checking in the book
+	 * @param ISBN of the book being checked in
+	 * @return true if checkin succeeds, false otherwise
+	 */
+	public boolean checkin(int uid, int ISBN)
+	{
+		return false;
+	}
+	
+	/**
+	 * Adds a user to the Users tables with the given information: uid, lastName, firstName, role
+	 * Needs to check that the accessID has the authorization to add users to the Users table
+	 * @param accessID the id of a person whose access level is higher than a patrons
+	 * @param uid user id of the user being added
+	 * @param lastName last name of the user
+	 * @param firstName first name of the user
+	 * @param role access level of the user
+	 * @return true if succeeds, false otherwise
+	 */
+	public boolean addUser(int accessID, int uid, String lastName, String firstName, Roles role) 
+	{
+		return false;
+	}
+	
+	/**
+	 * Looks up a user based off of uid
+	 * @param uid user id of person being looked up
+	 * @return ResultSet containing information of look up, or NULL if failed
+	 */
+	public ResultSet lookupUser(int uid) 
+	{
+		return null;
+	}
+	
+	/**
+	 * Looks up user(s) based off the user's name
+	 * If look up is with lastName only, set firstName to empty string
+	 * If look up is with firstName only, set lastName to empty string
+	 * Otherwise searching using both lastName and firstName
+	 * @param lastName of person being looked up
+	 * @param firstName of person being looked up
+	 * @return ResultSet containing information of look up, or NULL if failed
+	 */
+	public ResultSet lookupUser(String lastName, String firstName) 
+	{
+		return null;
+	}
+	
+	/**
+	 * Look up user(s) based off their role
+	 * @param role level of access into the database
+	 * @return ResultSet containing information of look up, or NULL if failed
+	 */
+	public ResultSet lookupUser(Roles role) 
+	{
+		return null;
+	}
+	
+	/**
+	 * Handles closing all connections to the database
+	 */
+	public abstract void closeConnection();
+}
--- src/BusinessObjects/User.java
+++ src/BusinessObjects/User.java
@@ -4,9 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class User 
+/**
+ * 
+ * @author zfalgout
+ *
+ * Handles User operations when dealing with the database
+ * Like connecting to the database, reading from database,
+ * and making changes to the database.
+ * When making changes to the database, a uid(user id) needs to be checked
+ * to make sure that user is allowed to make changes to the database
+ */
+
+public class User extends DBOperations
 {
 	private Connection connect = null;
+	//check if user is connected before read and writes to database
 	private boolean isConnected = false;
 	private String username, password;
 	public User(String username, String password)
@@ -15,6 +27,13 @@ public class User
 		this.password = password;
 	}
 	
+	/**
+	 * Handles trying to connect to the database
+	 * Will use the given user name and password for connections
+	 * NOTE: for visitors a default user name and password would be needed
+	 * to connect to database to allow reading from the database.
+	 * @return true if connection is successful, otherwise false
+	 */
 	public boolean connectToDB()
 	{
 		try 
@@ -28,7 +47,25 @@ public class User
 		}
 		return true;
 	}
-	
+
+
+	@Override
+	public boolean checkout(int uid, int ISBN) {
+		if(!isConnected) return false;
+		return false;
+	}
+
+	@Override
+	public boolean checkin(int uid, int ISBN) {
+		if(!isConnected) return false;
+		return false;
+	}
+
+	/**
+	 * Handles closing the connections to the database, and all related
+	 * database objects
+	 * If closing fails, the stack trace is printed
+	 */
 	public void closeConnection()
 	{
 		try 
@@ -39,4 +76,5 @@ public class User
 			e.printStackTrace();
 		}
 	}
+
 }
