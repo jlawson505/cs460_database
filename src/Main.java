8229874a728671bf7d3d257d8e970855e93e42b3
KurayamiSenpai <zfalgout>
2016-04-10 10:55:19 -0600

    Moved files to a source file since we didn't have that. Not adding classpath info so make sure classpath is set up correctly to run Java on your end

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by James Lawson on 4/8/2016.
+ *
+ * Gets the show started
+ */
+public class Main
+{
+
+  public static void main(String[] args)
+  {
+    new GUIFrame(new GUIPanel());
+  }
+}
